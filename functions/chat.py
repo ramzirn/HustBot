1b1981707d0dc8b6383f13d49b7e2f5770f043bd
Ramzile <ramzibouter6@gmail.com>
2023-09-04 14:21:48 +0100

    added chat file 2

--- functions/chat.py
+++ functions/chat.py
@@ -10,3 +10,18 @@ message='hello'
 while(message != ' '):
     message=input('-')
     getanswer(message)
+
+
+
+def supprimer_premier_mot(phrase):
+    # Divisez la phrase en mots en utilisant l'espace comme séparateur
+    mots = phrase.split()
+
+    # Vérifiez s'il y a au moins un mot dans la phrase
+    if len(mots) > 1:
+        # Supprimez le premier mot en utilisant un tranchage (slicing)
+        phrase_sans_premier_mot = ' '.join(mots[1:])
+        return phrase_sans_premier_mot
+    else:
+        # Si la phrase ne contient qu'un seul mot ou est vide, retournez une chaîne vide
+        return ""
